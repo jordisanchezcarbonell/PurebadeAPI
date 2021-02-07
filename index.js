5b1c0e6e3d062d5648402ddfdad8e34ae1b3d6d8
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2021-02-07 12:20:23 +0100

    upload

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 5000;
 
-// Listen on port 5000
+
+
+var MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kazukun:Kazukun23@cluster0.awxst.mongodb.net/Apiportfolio?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+
+/*
+*/
+  
+ /*
+ MongoClient
+.connect(uri, {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(`DB Connection Error: ${err.message}`);
+});
+*/
+app.get("/api/products", (req, res) => {
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("Apiportfolio");
+        //Find all documents in the customers collection:
+        dbo.collection("posts").find({}).toArray(function(err, result) {
+          if (err) throw err;
+        
+
+          db.close();
+          res.send(result);
+        });
+
+
+      });
+  });
+
+  // Listen on port 5000
 app.listen(port, () => {
   console.log(`Server is booming on port 5000
 Visit http://localhost:5000`);
@@ -12,3 +53,5 @@ Visit http://localhost:5000`);
 app.get('/',(req,res) => {
     res.send("This is a sample express app")
 })
+
+
